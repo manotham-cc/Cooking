e3ae58e883da2d4d2c0ad47df9f0540964a7baf9
manotham-cc <82450820+manotham-cc@users.noreply.github.com>
2024-06-05 22:29:05 +0700

    new

---
+++ client/src/post.js
@@ -0,0 +1,16 @@
+export default function Post(){
+    return(
+        <div className = "post">
+        <img src ="https://pinchofyum.com/wp-content/uploads/Two-Huge-Chocolate-Cookies-768x1025.jpg" alt = ""/>
+        <div className ="text">
+          <h2>Two Huge Double Chocolate Cookies</h2>
+          <p className = "info">
+            <a className='"author'>Dawid Paszko</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className = "summary">Just two huge double chocolate cookies to make you happy! Heavy and dense, fudgy and dark, deliciously thick, and sparkled with sugar. These are so good.</p>
+        </div>
+      </div>
+
+    );
+}
